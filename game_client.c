3c0e4961b5954a9f7e93107acc2a005df265a634
Sebastian <115455174+sebawoz02@users.noreply.github.com>
2023-04-30 14:57:27 +0200

    Finished client

--- game_client.c
+++ game_client.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+
+#include "minmax.h"
 
 int main(int argc, char *argv[])
 {
@@ -10,9 +13,11 @@ int main(int argc, char *argv[])
     struct sockaddr_in server_addr;
     char server_message[16], client_message[16];
 
-    int player, msg, move;
+    short msg, move;
+    char depth, player;
+    int alpha, beta;
 
-    if(argc != 4){
+    if(argc != 5){
         printf("Wrong number of arguments\n");
         return -1;
     }
@@ -54,6 +59,8 @@ int main(int argc, char *argv[])
 
     bool end_game = false;
     player = atoi(argv[3]);
+    depth = atoi(argv[4]);
+    initializeBoard();
 
     while (!end_game){
         memset(server_message, '\0', sizeof(server_message));
@@ -65,8 +72,43 @@ int main(int argc, char *argv[])
         msg = atoi(server_message);
         move = msg%100;
         msg = msg/100;
+        if(move != 0){
+            makeMove(move, 3 - player);
+            printBoard();
+        }
+        if( (msg==0) || (msg==6) ) {
+            printf("Your move: ");
+            if( fgets(client_message, sizeof(client_message), stdin)==NULL ) {
+                printf("Error while reading move\n");
+                return -1;
+            }
+            move = atoi(client_message);
+            makeMove(move, player);
+            printBoard();
+            memset(client_message, '\0', sizeof(client_message));
+            // Find the best next move
+            sprintf(client_message, "%d", minmax(board, depth, alpha, beta, player));
+
+            if( send(socket_desc, client_message, strlen(client_message), 0)<0 ) {
+                printf("Unable to send message\n");
+                return -1;
+            }
+            printf("Client message: %s\n", client_message);
+        }
+        else {
+            end_game = true;
+            switch( msg ) {
+                case 1 : printf("You won.\n"); break;
+                case 2 : printf("You lost.\n"); break;
+                case 3 : printf("Draw.\n"); break;
+                case 4 : printf("You won. Opponent error.\n"); break;
+                case 5 : printf("You lost. Your error.\n"); break;
+            }
+        }
+
+
     }
 
-
+    close(socket_desc);
     return 0;
 }
